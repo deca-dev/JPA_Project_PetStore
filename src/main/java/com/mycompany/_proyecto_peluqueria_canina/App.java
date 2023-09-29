c387a5d38f4e34a3e1d520a9cd7b99bff0b24392
David Castro <davidernesto.castroanaya@gmail.com>
2023-09-28 22:09:39 -0600

    "Adding edit and eliminate to CRUD"

---
+++ src/main/java/com/mycompany/_proyecto_peluqueria_canina/App.java
@@ -0,0 +1,12 @@
+package com.mycompany._proyecto_peluqueria_canina;
+
+import com.mycompany._proyecto_peluqueria_canina.igu.Principal;
+
+public class App {
+
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+    }
+}
